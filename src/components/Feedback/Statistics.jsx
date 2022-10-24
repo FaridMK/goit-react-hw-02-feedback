6d03717a8146c58648fd42a1528ae4af59faaeaa
Farid Suleymanov <neu.apply@gmail.com>
2022-10-24 20:52:18 +0400

    fixed

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -3,19 +3,25 @@ import { FeedbackResult, FeedbackStatistics } from './Feedback.styled';
 
 const Statistics = ({ totalFeedbacks, positivePercentage, up, zero, down }) => (
   <div>
-    <FeedbackStatistics>
-      <ul>Statistics</ul>
-      <li>Good: {up}</li>
-      <li>Neutral: {zero}</li>
-      <li>Bad: {down} </li>
-    </FeedbackStatistics>
-    <FeedbackResult>
-      <p>TOTAL: {totalFeedbacks}</p>
-      <p>
-        Positive Feedbacks in:
-        {positivePercentage}
-      </p>
-    </FeedbackResult>
+    {totalFeedbacks === 0 ? (
+      <p>There is no feedback</p>
+    ) : (
+      <>
+        <FeedbackStatistics>
+          <ul>Statistics</ul>
+          <li>Good: {up}</li>
+          <li>Neutral: {zero}</li>
+          <li>Bad: {down} </li>
+        </FeedbackStatistics>
+        <FeedbackResult>
+          <p>TOTAL: {totalFeedbacks}</p>
+          <p>
+            Positive Feedbacks in:
+            {positivePercentage}
+          </p>
+        </FeedbackResult>
+      </>
+    )}
   </div>
 );
 
